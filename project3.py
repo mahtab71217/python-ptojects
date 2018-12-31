cb9b1e4b05f3ccb5f9f2e1a67066fbc95849420d
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2018-12-31 23:28:13 +0330

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 23 22:07:18 2018
+
+@author: mahtab
+"""
+
+#project3
+number= eval(input('Enter the number: '))
+out={}
+for i in range(1,number+1):
+    out[i]=i*i
+    
+print(out)
+
+
+
