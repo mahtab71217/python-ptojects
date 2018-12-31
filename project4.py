8497483f4abef919e5f53bd26722b6a8cdfc005b
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2018-12-31 23:28:28 +0330

    Add files via upload

---
+++ project4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 23:07:51 2018
+
+@author: mahtab
+"""
+#project4
+number=input('Enter the sequence of numbers: ')
+number=number.split(',')
+List_sequence=list(number)
+Tuple_sequence=tuple(number)
+print(List_sequence)
+print(Tuple_sequence)
