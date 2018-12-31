abe7d8b8f80b49802460f0c09ff82fa88b70c0d1
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2018-12-31 23:27:57 +0330

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 23 21:57:07 2018
+
+@author: mahtab
+"""
+
+#project2
+number= eval(input('Enter the number: '))
+out=1
+for i in range(1,number+1):
+    out=out*i
+    
+print(out)
+    
