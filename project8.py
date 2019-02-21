a8cae0a59b9180f479db903662ad19f71473364a
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2019-02-21 23:35:24 +0330

    Update project8.py

--- project8.py
+++ project8.py
@@ -2,10 +2,17 @@
 """
 Created on Thu Feb 21 23:16:09 2019
 
-@author: mahtab
+@author: mahtab faraji
+www.onlinebme.com
 """
+#Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+#Suppose the following input is supplied to the program:
+#without,hello,bag,world
+#Then, the output should be:
+#bag,hello,without,world
 
 x=input('Enter the first number: ')
 my_list=x.split(',')
 sort_list=sorted(my_list)
-
+new_str=','.join(sort_list)
+print(new_str)
