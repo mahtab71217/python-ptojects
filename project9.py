79f6ff769eee7088b5ff2dd622cc1bcf343b6f47
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2019-02-24 16:58:36 +0330

    Update project9.py

--- project9.py
+++ project9.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 23 18:04:55 2019
+#Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+#Suppose the following input is supplied to the program:
+#Hello world
+#Practice makes perfect
+#Then, the output should be:
+#HELLO WORLD
+#PRACTICE MAKES PERFECT
+#Created on Sat Feb 23 18:04:55 2019
 
 @author: mahtab faraji
 www.onlinebme.com
