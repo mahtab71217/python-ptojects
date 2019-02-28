742d665ca0e11513c08adc6f7159996376a7f631
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2019-02-28 22:50:46 +0330

    Update project11.py

--- project11.py
+++ project11.py
@@ -1,8 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 28 22:17:31 2019
+#Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+#Example:
+#0100,0011,1010,1001
+#Then the output should be:
+#1010
+#Created on Thu Feb 28 22:17:31 2019
 
-@author: mahtab
+#@author: mahtab faraji
+#www.onlinebme.com
 """
 
 sen=input('Enter your sentence: ')
