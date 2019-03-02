03e07514d45313a69176da4ee50f39879f81860e
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2019-03-02 12:26:14 +0330

    Update project12.py

--- project12.py
+++ project12.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """
+Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number.
+The numbers obtained should be printed in a comma-separated sequence on a single line.
 Created on Thu Feb 28 22:53:16 2019
 
-@author: mahtab
+@author: mahtab faraji
+www.onlinebme.com
 """
 out=[]
 for i in range(1000,3001):
