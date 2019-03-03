29f0225b1124ebeab6d12702dcb715e1e535456c
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2019-03-03 11:48:12 +0330

    Update project16.py

--- project16.py
+++ project16.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
+"""Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers.
+Suppose the following input is supplied to the program:
+1,2,3,4,5,6,7,8,9
+Then, the output should be:
+1,3,5,7,9
+
 Created on Sun Mar  3 11:37:05 2019
 
-@author: mahtab
+@author: mahtab faraji
+www.onlinebme.com
 """
 
 seq=input('Enter the sequence of numbers: ')
