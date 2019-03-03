156ee7de2b65c009165183f79ea803c8c4a96cf0
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2019-03-03 11:31:44 +0330

    Update project15.py

--- project15.py
+++ project15.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
+"""Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+Suppose the following input is supplied to the program:
+9
+Then, the output should be:
+11106
 Created on Sun Mar  3 11:20:39 2019
 
-@author: mahtab
+@author: mahtab faraji
+www.onlinebme.com
 """
 
 a=input('Enter the value as a: ')
