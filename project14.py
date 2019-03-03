f0887b7dbab4e12f6bf4025916b37ac80aa68daf
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2019-03-03 11:20:15 +0330

    Update project14.py

--- project14.py
+++ project14.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
+"""Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+Suppose the following input is supplied to the program:
+Hello world!
+Then, the output should be:
+UPPER CASE 1
+LOWER CASE 9
 Created on Sun Mar  3 11:09:56 2019
 
-@author: mahtab
+@author: mahtab faraji
+www.onlinebme.com
 """
 
 sen=input('Enter your sen: ')
