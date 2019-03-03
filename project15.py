21ae301a3a0f29e22ae8390cca2b1970911b1c28
mahtab71217 <45668993+mahtab71217@users.noreply.github.com>
2019-03-03 11:31:06 +0330

    Add files via upload

---
+++ project15.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 11:20:39 2019
+
+@author: mahtab
+"""
+
+a=input('Enter the value as a: ')
+
+out=eval(a)+eval(a+a)+eval(a+a+a)+eval(a+a+a+a)
+print(out)
+    
+    
